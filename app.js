891cc35d874bf1115f92fed1035e87030afe5f10
Jorge Bolpe <ygd182@gmail.com>
2018-02-19 17:52:53 -0300

    fixed error log

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // render the error page
-  console.log(err);
+  console.log(err.message);
   res.status(err.status || 500);
   res.json({error: err});
 });
