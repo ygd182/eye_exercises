4d734ecb8c0c66024eb5a2f76afbaee647cc4378
Jorge Bolpe <ygd182@gmail.com>
2018-02-19 20:33:50 -0300

    added dropdown role selection wip

--- public/js/admin.js
+++ public/js/admin.js
@@ -62,6 +62,19 @@
 			}
 		});
 
+		$( document.body ).on( 'click', '#role-selection-dropdown-menu li', function( event ) {
+
+	      var $target = $( event.currentTarget );
+
+	      $target.closest( '.btn-group' )
+	         .find( '[data-bind="label"]' ).text( $target.text() )
+	            .end()
+	         .children( '.dropdown-toggle' ).dropdown( 'toggle' );
+
+	      return false;
+
+	   });
+
 	}
 
 
